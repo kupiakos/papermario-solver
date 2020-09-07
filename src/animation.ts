5378e23fb75fd42a120c8e8dca600fb66a8aea7c
Alyssa Haroldsen <kupiakos@gmail.com>
2020-09-06 21:04:10 -0700

    Create a basic animation library
    Yeah, I know, there are libraries that do this. Sue me, I like to
    program my own stuff sometimes.

---
+++ src/animation.ts
@@ -0,0 +1,133 @@
+
+export interface Animatable {
+  // Draws the frame given the current time t.
+  // Returns whether to recall this Animatable on the next frame.
+  drawFrame(t: number): boolean;
+  
+  // Is this animation currently playing?
+  // More precisely, has the last frame of this animation finished?
+  isPlaying(): boolean;
+  
+  // Starts playing the animation.
+  play(): void;
+  
+  // Stops playing the animation.
+  stop(): void;
+}
+
+// A callback called on each frame with the amount
+// progressed through the animation. amount is a number
+// in the range [0, 1] that indicates how far through the
+// animation to draw.
+export type FrameCallback = (amount: number) => void;
+export type AnimationFinishedCallback = () => void;
+export class Animation implements Animatable {
+  duration_sec: number;
+  private start: number;
+  private onframe: FrameCallback;
+  private onfinish?: AnimationFinishedCallback;
+
+  constructor(
+      duration_sec: number,
+      onframe: FrameCallback,
+      onfinish?: AnimationFinishedCallback) {
+    if (duration_sec <= 0) {
+      throw 'duration_sec <= 0';
+    }
+    this.duration_sec = duration_sec;
+    this.onframe = onframe;
+    this.onfinish = onfinish;
+  }
+
+  isPlaying(): boolean {
+    return AnimationManager.getInstance().isScheduled(this);
+  }
+
+  play(duration_sec: number = this.duration_sec): void {
+    if (duration_sec <= 0) {
+      throw 'duration_sec <= 0';
+    }
+    this.duration_sec = duration_sec;
+    this.start = performance.now();
+    AnimationManager.getInstance().schedule(this);
+  }
+
+  stop(): void {
+    AnimationManager.getInstance().unschedule(this);
+    if (this.onfinish) {
+      this.onfinish();
+    }
+  }
+
+  drawFrame(t: number): boolean {
+    // console.log('drawFrame', t);
+    let amount = (t - this.start) / (this.duration_sec * 1000);
+    this.onframe(Math.min(Math.max(0, amount), 1));
+    if (amount >= 1) {
+      if (this.onfinish) {
+        this.onfinish();
+      }
+      return false;
+    }
+    return true;
+  }
+}
+
+export class AnimationManager {
+  private animation_frame_id?: number;
+  private playing: Set<Animatable>;
+  private static instance: AnimationManager;
+
+  static getInstance(): AnimationManager {
+    if (!this.instance) {
+      this.instance = new AnimationManager();
+    }
+    return this.instance;
+  }
+
+  private constructor() {
+    this.animation_frame_id = null;
+    this.playing = new Set<Animatable>();
+  }
+
+  schedule(a: Animatable) {
+    this.playing.add(a);
+    this.startAnimating();
+  }
+
+  unschedule(a: Animatable) {
+    this.playing.delete(a);
+  }
+
+  isScheduled(a: Animatable): boolean {
+    return this.playing.has(a);
+  }
+
+  private animationFrame(t: number) {
+    for (let a of Array.from(this.playing)) {
+      if (!a.drawFrame.call(a, t)) {
+        this.playing.delete(a);
+      }
+    }
+    this.animation_frame_id = null;
+    if (this.playing.size > 0) {
+      this.animation_frame_id = window.requestAnimationFrame(
+        this.animationFrame.bind(this));
+    }
+  }
+
+  private startAnimating() {
+    if (this.animation_frame_id === null) {
+      this.animation_frame_id = window.requestAnimationFrame(
+        this.animationFrame.bind(this));
+    }
+  }
+
+  cancelAnimating() {
+    if (this.animation_frame_id !== null) {
+      window.cancelAnimationFrame(this.animation_frame_id);
+      this.animation_frame_id = null;
+    }
+  }
+}
+
